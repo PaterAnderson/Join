44f7ecbfcf37d327d65167b8b7e448afb4fc949d
Lukasz Szymczak <lukas.sz.br@gmail.com>
2024-09-05 23:49:35 +0200

    login and register

--- script/login.js
+++ script/login.js
@@ -1,9 +1,23 @@
 
+const BASE_URL = "https://join-projekt-85028-default-rtdb.europe-west1.firebasedatabase.app/";
+
+
+/**
+ * 
+ *   first check whether redirection to index.html is necessary
+ * 
+ */
 function loginStart() {
 
     checkReturnToIndexHTML();
 }
 
+
+/**
+ * 
+ *   check whether redirection to index.html is necessary
+ *  
+ */
 function checkReturnToIndexHTML() {
 
     const startedJoin = sessionStorage.getItem('started');
@@ -15,13 +29,96 @@ function checkReturnToIndexHTML() {
     } else document.getElementById('main_div').classList.remove('display-none');
 }
 
-// Funktion, die beim Formular-Submit aufgerufen wird
+
+/**
+ * 
+ *   function called when form onsubmit
+ *  
+ */
 function loginSubmit() {
 
-    savePassword();
-    //window.location.href = '../html/summary.html';
+    email = document.getElementById('email').value;
+    password = document.getElementById('password').value;
+
+    checkEmailAndPasswordForLogin(email, password);
 };
 
+
+/**
+ *   check email and password for log in
+ * 
+ *   @param {string} email 
+ *   @param {string} password 
+ */
+async function checkEmailAndPasswordForLogin(email, password) {
+
+    try {
+        // Hole die Daten von Firebase
+        let response = await fetch(BASE_URL + ".json");
+
+        if (!response.ok) {
+            throw new Error('Netzwerkantwort war nicht ok.');
+        }
+
+        let responseToJson = await response.json();
+
+        // Durchsuche die JSON-Daten nach existierenden Benutzern oder E-Mails
+
+        for (const key in responseToJson) {
+
+            if ((responseToJson[key].email === email) && (responseToJson[key].password === password)) userLoginStart(key);
+        }
+
+        showNotice();
+
+    } catch (error) { console.error('Fehler beim Abrufen oder Verarbeiten der Daten:', error) }
+}
+
+
+/**
+ * 
+ *   show notice under password input
+ * 
+ */
+function showNotice() {
+
+    document.getElementById('password_notice').classList.remove('display-none');
+
+    document.getElementById('input_outside1').style = "border: 1px solid #FF8190";
+    document.getElementById('input_outside2').style = "border: 1px solid #FF8190";
+}
+
+
+/**
+ * 
+ *   if input focused - hide notice, reset border for inputs 
+ * 
+ */
+function inputIsOnFocus() {
+
+    document.getElementById('password_notice').classList.add('display-none');
+
+    document.getElementById('input_outside1').style = "";
+    document.getElementById('input_outside2').style = "";
+}
+
+
+/**
+ *   delete content of password input if focused
+ * 
+ *   @param {this} passwordRef 
+ */
+function deleteContent(passwordRef) {
+
+    passwordRef.value = "";
+}
+
+
+/**
+ * 
+ *   if allowed by check box 'remember me' calling saving passwords
+ * 
+ */
 function savePassword() {
 
     const rememberMe = document.getElementById('rememberMe');
@@ -34,18 +131,56 @@ function savePassword() {
                 id: form.elements.email.value,
                 password: form.elements.password.value
             });
+
             navigator.credentials.store(cred).then(function () {
-                //          console.log('Credentials stored successfully!');
-                //          form.submit(); // Formular nach dem Speichern abschicken
-            }).catch(function (error) {
-                console.error('Error storing credentials:', error);
-            });
+                //console.log('Credentials stored successfully!');
+            }).catch(function (error) { console.error('Error storing credentials:', error) });
         }
     }
 }
 
+
+/**
+ * 
+ *   forward to register.html
+ * 
+ */
 function toRegister() {
 
     window.location.href = '../html/register.html';
 }
+
+
+/**
+ * 
+ *   forward to summary.html with guest user 
+ * 
+ */
+function guestLoginStart() {
+
+    sessionStorage.setItem('user', 'guest');
+
+
+    alert('guest login!');
+}
+
+
+/**
+ *   if allowed by check box 'remember me' calling saving passwords, forward to summary.html with logged in user
+ * 
+ *   @param {string} user 
+ */
+function userLoginStart(user) {
+
+    savePassword();
+    sessionStorage.setItem('user', user);
+
+
+    alert('user login!' + ' ' + user);
+
+
+    window.location.href = '../html/login.html';
+}
+
+
 
--- script/register.js
+++ script/register.js
@@ -1,13 +1,25 @@
 
 const BASE_URL = "https://join-projekt-85028-default-rtdb.europe-west1.firebasedatabase.app/";
 
+let noticeIndex;
+
+
+/**
+ * 
+ *   function called at start
+ * 
+ */
 function registerStart() {
 
     checkReturnToIndexHTML();
-
-
 }
 
+
+/**
+ * 
+ *   check whether redirection to index.html is necessary
+ * 
+ */
 function checkReturnToIndexHTML() {
 
     const startedJoin = sessionStorage.getItem('started');
@@ -19,74 +31,240 @@ function checkReturnToIndexHTML() {
     } else document.getElementById('main_div').classList.remove('display-none');
 }
 
+
+/**
+ * 
+ *   forward to login.html
+ * 
+ */
 function returnToLogin() {
 
     window.location.href = '../html/login.html';
 }
 
-function registerSubmit() {
 
+/**
+ * 
+ *   button toggle between active and inactive
+ * 
+ */
+function toggleActivateButton() {
+
+    const registerButton = document.getElementById('register_sign_up_button');
+
+    if (registerButton.disabled) { registerButton.disabled = false } else registerButton.disabled = true;
+}
+
+
+/**
+ * 
+ *   function that is called, button sign up is pressed
+ * 
+ */
+function registerSubmit() {
 
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirm_password').value;
-
-
+    const user = document.getElementById('user').value;
+    const email = document.getElementById('email').value;
 
     if (password == confirmPassword) {
 
-
-
-        const user = document.getElementById('user').value;
-        const email = document.getElementById('email').value;
-
-
-        putData(`/${user}`, { "email": email });
-        putData(`/${user}`, { "password": password });
-
-        
+        checkEmailAndUserForRegister(user, email, password);
 
     } else {
 
-        document.getElementById('password_notice').classList.remove('display-none');
-    }    
+        writeNotice("Your passwords don't match. Please try again.");
+        noticeIndex = 2;
+
+        document.getElementById('input_border3').style = "border: 1px solid #FF8190";
+        document.getElementById('input_border4').style = "border: 1px solid #FF8190";
+    }
 }
 
+
+/**
+ *   write notice under password input
+ * 
+ *   @param {string} notice 
+ */
+function writeNotice(notice) {
+
+    document.getElementById('password_notice').innerText = notice;
+    document.getElementById('password_notice').classList.remove('display-none');
+}
+
+
+/**
+ *   add new user
+ * 
+ *   @param {string} user 
+ *   @param {string} email 
+ *   @param {string} password 
+ */
+function addNewUser(user, email, password) {
+
+    putData(`/${user}`, { "email": email, "password": password });
+    animationSuccess();
+
+    setTimeout(() => window.location.href = '../html/login.html', 2000);
+}
+
+
+/**
+ * 
+ *   animation for sign up successfully
+ * 
+ */
+function animationSuccess() {
+
+    document.getElementById('overlay').classList.remove('display-none');
+    document.getElementById('success').classList.add('success2');
+}
+
+
+/**
+ * 
+ *   input for user on focus, reset border of input, hide notice under password input
+ * 
+ */
+function inputForUserOnFocus() {
+
+    document.getElementById('input_border1').style = "";
+    document.getElementById('input_border2').style = "";
+
+    if (noticeIndex == 1) document.getElementById('password_notice').classList.add('display-none');
+}
+
+
+/**
+ * 
+ *   input for email on focus, reset border of input, hide notice under password input
+ * 
+ */
+function inputForEmailOnFocus() {
+
+    document.getElementById('input_border1').style = "";
+    document.getElementById('input_border2').style = "";
+
+    if (noticeIndex == 1) document.getElementById('password_notice').classList.add('display-none');
+}
+
+
+/**
+ * 
+ *   input for password on focus, reset border of input, hide notice under password input
+ * 
+ */
 function inputForPasswordOnFocus() {
 
     const password = document.getElementById('password');
     password.value = "";
 
-    document.getElementById('password_notice').classList.add('display-none');
+    document.getElementById('input_border3').style = "";
+    document.getElementById('input_border4').style = "";
 
-    
+    if (noticeIndex == 2) document.getElementById('password_notice').classList.add('display-none');
 }
 
+
+/**
+ * 
+ *   input for confirm password on focus, reset border of input, hide notice under password input
+ * 
+ */
 function inputForConfirmPasswordOnFocus() {
 
     const confirmPassword = document.getElementById('confirm_password');
     confirmPassword.value = "";
 
-    document.getElementById('password_notice').classList.add('display-none');
+    document.getElementById('input_border3').style = "";
+    document.getElementById('input_border4').style = "";
 
-    
+    if (noticeIndex == 2) document.getElementById('password_notice').classList.add('display-none');
 }
 
+
+/**
+ *   put data on firebase
+
+ * 
+ *   @param {string} path 
+ *   @param {Array} data 
+ */
 async function putData(path = "", data = {}) {
 
     let response = await fetch(BASE_URL + path + ".json", {
 
         method: "PUT",
-
         header: {
-
             "Content-Type": "aplication/json"
         },
 
         body: JSON.stringify(data)
-
     });
 
     responseToJson = await response.json();
-
-    console.log(responseToJson);
 }
+
+
+/**
+ *   check email and user for register
+ * 
+ *   @param {string} user 
+ *   @param {string} email 
+ *   @param {string} password 
+ */
+async function checkEmailAndUserForRegister(user, email, password) {
+
+    try {
+        // Hole die Daten von Firebase
+        let response = await fetch(BASE_URL + ".json");
+
+        if (!response.ok) {
+            throw new Error('Netzwerkantwort war nicht ok.');
+        }
+
+        let responseToJson = await response.json();
+
+        // Durchsuche die JSON-Daten nach existierenden Benutzern oder E-Mails
+        let isUserOrEmailExist = false;
+
+        for (const key in responseToJson) {
+            if (responseToJson[key].user === user) isUserOrEmailExist = true;
+            if (responseToJson[key].email === email) isUserOrEmailExist = true;
+        }
+
+        writeMessageOrRegisterNewUser(user, email, password, isUserOrEmailExist);
+
+    } catch (error) { console.error('Fehler beim Abrufen oder Verarbeiten der Daten:', error) }
+}
+
+
+/**
+ *   write message or register new user
+ * 
+ *   @param {string} user 
+ *   @param {string} email 
+ *   @param {string} password 
+ *   @param {boolean} isUserOrEmailExist 
+ */
+function writeMessageOrRegisterNewUser(user, email, password, isUserOrEmailExist) {
+
+    // Logge aus, falls Benutzer oder E-Mail bereits vorhanden sind
+    if (isUserOrEmailExist) {
+
+        writeNotice("User or email already exists.");
+        noticeIndex = 1;
+
+        document.getElementById('input_border1').style = "border: 1px solid #FF8190";
+        document.getElementById('input_border2').style = "border: 1px solid #FF8190";
+
+    } else {
+
+        addNewUser(user, email, password);
+    }
+}
+
+
+
