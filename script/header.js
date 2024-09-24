15b8862fa26a363e95bb25757de9eda82fe0eccd
Lukasz Szymczak <lukas.sz.br@gmail.com>
2024-09-24 14:50:52 +0200

    menu_head_resp.css aktualisiert

--- script/header.js
+++ script/header.js
@@ -1,8 +1,33 @@
+
+if (!sessionStorage.getItem('user')) window.location.href = '../index.html';
+
 function openUserMenu(){
-    document.getElementById('user-nav').classList.toggle('d-none')
-    document.getElementById('user-icon').classList.toggle('icon-color-after-click')
+    document.getElementById('user-nav').classList.toggle('d-none');
+    //document.getElementById('user-icon').classList.toggle('icon-color-after-click');
+}
+
+function closeUserMenu(){
+    document.getElementById('user-nav').classList.add('d-none');
+    //document.getElementById('user-icon').classList.add('icon-color-after-click');
+}
+
+function stopProp(event) {
+
+    event.stopPropagation();
 }
 
 function userIconName(){
-    document.getElementById('user-icon').innerText = 'SM'
+    document.getElementById('user-icon').innerText = calculateInitialsHeader(sessionStorage.getItem('user'));    
 }
+
+
+/**
+ * Function to calculate the initials
+ * 
+ * @param {string} name
+ */
+const calculateInitialsHeader = (name) => {
+    const [firstName, lastName] = name.split(' ');
+    if (!lastName) return `${firstName.charAt(0)}`;
+    return `${firstName.charAt(0)}${lastName.charAt(0)}`;
+};
