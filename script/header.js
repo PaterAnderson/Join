cec833241f9a39a029867e68103e23615ca09a83
Lukasz Szymczak <lukas.sz.br@gmail.com>
2024-10-06 15:53:07 +0200

    contacts responsive, user-icon-menu mobile

--- script/header.js
+++ script/header.js
@@ -7,9 +7,17 @@ if (!sessionStorage.getItem('user')) window.location.href = '../index.html';
  * open user menu
  * 
  */
-function openUserMenu(){
-    document.getElementById('user-nav').classList.toggle('d-none');
-    //document.getElementById('user-icon').classList.toggle('icon-color-after-click');
+function openUserMenu() {
+
+    if (!(window.innerWidth <= 600)) {
+
+        document.getElementById('user-nav').classList.toggle('d-none');
+        //document.getElementById('user-icon').classList.toggle('icon-color-after-click');
+
+    } else {
+
+        document.getElementById('user-nav').classList.toggle('u-i-m-sh-m');
+    }
 }
 
 
@@ -18,9 +26,17 @@ function openUserMenu(){
  * close user menu
  * 
  */
-function closeUserMenu(){
-    document.getElementById('user-nav').classList.add('d-none');
-    //document.getElementById('user-icon').classList.add('icon-color-after-click');
+function closeUserMenu() {
+
+    if (!(window.innerWidth <= 600)) {
+
+        document.getElementById('user-nav').classList.add('d-none');
+        //document.getElementById('user-icon').classList.add('icon-color-after-click');
+
+    } else {
+
+        document.getElementById('user-nav').classList.remove('u-i-m-sh-m');
+    }
 }
 
 
@@ -40,8 +56,8 @@ function stopProp(event) {
  * funkction for user icon initials
  * 
  */
-function userIconName(){
-    document.getElementById('user-icon').innerText = calculateInitialsHeader(sessionStorage.getItem('user'));    
+function userIconName() {
+    document.getElementById('user-icon').innerText = calculateInitialsHeader(sessionStorage.getItem('user'));
 }
 
 
@@ -54,4 +70,7 @@ const calculateInitialsHeader = (name) => {
     const [firstName, lastName] = name.split(' ');
     if (!lastName) return `${firstName.charAt(0)}`;
     return `${firstName.charAt(0)}${lastName.charAt(0)}`;
-};
+}
+
+
+
